80beea8116a357d171e0e76ad7789abc4f2fcb66
Justin Fiset <justinfiset@hotmail.fr>
2025-02-24 21:35:55 -0500

    Création des fichiers initiaux et prototype de UI initial

---
+++ ressources/js/terminal.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    logInfo("Bienvenue sur le panneau de contrôle de la Bash Space Program Agency.");
+    logWarning("Entrez la commande 'HELP' pour plus d'information.");
+    logError("CECI EST UN TEST D'ERREUR À ENLEVER APRÈS LE CHARGEMENT DE LA PAGE");
+});
+
+function log(text, color) {
+    const terminal = document.getElementById("terminal-content");
+
+    const newText = document.createElement("p");
+    newText.style.color = color;
+    newText.innerHTML = text;
+
+    terminal.append(newText);
+}
+
+function logInfo(text)    { log(text, "greenyellow"); }
+function logWarning(text) { log(text, "orange");      }
+function logError(text)   { log(text, "orangered");   }
