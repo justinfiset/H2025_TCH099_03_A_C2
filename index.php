76008066cf5c68f1cbe114a1bcf53bd42efe9cc3
AimeMelancon <aimemelancon@gmail.com>
2025-04-09 10:44:27 -0400

    favicon

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $_SESSION["time"] = $dateLocale;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <title>BSP Terminal</title>
 
     <link rel="stylesheet" href="./ressources/css/normalize.css">
