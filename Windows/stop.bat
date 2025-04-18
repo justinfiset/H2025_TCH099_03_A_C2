c706c2404e0939b1d5c31184a09e7a41f04d8706
AimeMelancon <aimemelancon@gmail.com>
2025-04-18 12:28:17 -0400

    correction de fichier

--- Windows/stop.bat
+++ Windows/stop.bat
@@ -1,11 +1,1 @@
-#!/bin/bash
-# Vérifier si localhost:9000 est ouvert et utilisé actuellement. 
-# lsof = List Open Files = permet de regarder les fichiers ouverts par un processus.
-# Fait la boucle à l'infini tout aussi longtemps que le port est utilisé.
-
-while lsof -i :9000 > /dev/null; do
-  sleep 1
-done
-
-# Arrêter les conteneurs
 docker-compose down
